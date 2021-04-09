a969fd2efc6173b1db50ce087b7e4d7395a021f1
Edson Shideki Kokado <eskokado@gmail.com>
2021-04-09 15:37:33 -0300

    add resources Job and Profile

---
+++ src/app/resources/jobs.ts
@@ -0,0 +1,14 @@
+﻿export interface Job {
+  id: number;
+  name: string;
+  daily_hours: number;
+  total_hours: number;
+  created_at: number;
+}
+
+export interface saveJob {
+  name: string;
+  daily_hours: number;
+  total_hours: number;
+  created_at: number;
+}
---
+++ src/app/resources/profiles.ts
@@ -0,0 +1,20 @@
+﻿export interface Profile {
+  id: number;
+  name: string;
+  avatar: string;
+  monthly_budget: number;
+  days_per_week: number;
+  hours_per_day: number;
+  vacation_per_year: number;
+  value_hour: number;
+}
+
+export interface saveProfile {
+  name: string;
+  avatar: string;
+  monthly_budget: number;
+  days_per_week: number;
+  hours_per_day: number;
+  vacation_per_year: number;
+  value_hour: number;
+}
