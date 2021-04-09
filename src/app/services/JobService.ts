83cd3192920268cc985483fad393710ba81fe5fa
Edson Shideki Kokado <eskokado@gmail.com>
2021-04-09 15:37:55 -0300

    add services Job and Profile

---
+++ src/app/services/JobService.ts
@@ -0,0 +1,38 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { saveJob, Job } from '../resources/jobs';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class JobService {
+  private jobsUrl = 'http://localhost:3000/api/v1/jobs';
+  private headers = new Headers({
+    'Access-Control-Allow-Origin': '*',
+    Accepts: 'application/json',
+    'Content-Type': 'application/json',
+  });
+
+  public constructor(private http: HttpClient) {}
+
+  listJobs(): Observable<Job[]> {
+    return this.http.get<Job[]>(this.jobsUrl);
+  }
+
+  getJob(jobId: string) {
+    return this.http.get<Job>(`${this.jobsUrl}/${jobId}`);
+  }
+
+  insertJob(job: Job): Observable<Job> {
+    return this.http.post<Job>(this.jobsUrl, job);
+  }
+
+  updateJob(id: number, job: saveJob): Observable<Job> {
+    const url = `${this.jobsUrl}/${id}`;
+    return this.http.put<Job>(url, JSON.stringify(job));
+  }
+
+  deleteJob(id: number): Observable<{}> {
+    const url = `${this.jobsUrl}/${id}`;
+    return this.http.delete(url);
+  }
+}
---
+++ src/app/services/ProfileService.ts
@@ -0,0 +1,38 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Profile, saveProfile } from '../resources/profiles';
+
+@Injectable()
+export class ProfileService {
+  private profileUrl = 'http://localhost:3000/api/v1/profiles';
+  private headers = new Headers({
+    'Access-Control-Allow-Origin': '*',
+    Accepts: 'application/json',
+    'Content-Type': 'application/json',
+  });
+
+  public constructor(private http: HttpClient) {}
+
+  listProfiles(): Observable<Profile[]> {
+    return this.http.get<Profile[]>(this.profileUrl);
+  }
+
+  getJob(profileId: string) {
+    return this.http.get<Profile>(`${this.profileUrl}/${profileId}`);
+  }
+
+  insertJob(profile: Profile): Observable<Profile> {
+    return this.http.post<Profile>(this.profileUrl, profile);
+  }
+
+  updateJob(id: number, profile: saveProfile): Observable<Profile> {
+    const url = `${this.profileUrl}/${id}`;
+    return this.http.put<Profile>(url, JSON.stringify(profile));
+  }
+
+  deleteJob(id: number): Observable<{}> {
+    const url = `${this.profileUrl}/${id}`;
+    return this.http.delete(url);
+  }
+}
