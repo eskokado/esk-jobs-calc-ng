38dcdb0188105c28804ed1c2df54cd6eea32d825
Edson Shideki Kokado <eskokado@gmail.com>
2021-04-10 05:19:43 -0300

    add component routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { JobEditComponent } from './pages/job-edit/job-edit.component';
+import { JobComponent } from './pages/job/job.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'job', component: JobComponent },
+  { path: 'job-edit', component: JobEditComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
