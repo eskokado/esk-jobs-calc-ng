2baa35641597ee4fa18a9f676cc9adbebc3c4b51
Edson Shideki Kokado <eskokado@gmail.com>
2021-04-12 14:09:39 -0300

    add Job and Profile models

---
+++ src/app/models/job.ts
@@ -0,0 +1,9 @@
+﻿export class Job {
+  constructor(
+    public id: number,
+    public name: string,
+    public daily_hours: number,
+    public total_hours: number,
+    public created_at: number
+  ) {}
+}
---
+++ src/app/models/profile.ts
@@ -0,0 +1,12 @@
+﻿export class Profile {
+  constructor(
+    public id: number,
+    public name: string,
+    public avatar: string,
+    public monthly_budget: number,
+    public days_per_week: number,
+    public hours_per_day: number,
+    public vacation_per_year: number,
+    public value_hour: number
+  ) {}
+}
